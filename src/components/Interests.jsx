0df6adc308f80f8e1d71004570252df421f08103
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-27 15:53:14 +0200

    feat: add interests component
    - add interests section with heading and styled list
    - match padding and typography from about component

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,3 +1,16 @@
 export default function Interests() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col justify-start px-6 pt-6">
+        <h2 className="text-lg font-bold mb-2">Interests</h2>
+        <ul className="font-light text-sm opacity-75">
+          <li>&bull; Plant-based enthusiast.</li>
+          <li>&bull; Music.</li>
+          <li>&bull; Reader.</li>
+          <li>&bull; Travel geek.</li>
+          <li>&bull; Coffee.</li>
+        </ul>
+      </div>
+    </>
+  );
 }
