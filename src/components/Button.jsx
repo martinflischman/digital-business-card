b01b5eba156d85c6e821993f399ba5f55b77f321
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-27 16:55:20 +0200

    feat: add mailto link to email button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,13 @@ import { FaEnvelope } from "react-icons/fa";
 
 export default function Button() {
   return (
-    <>
-      <div className="flex justify-center">
-        <button className="btn btn-ghost bg-gray-100 text-gray-800 w-4/5">
+    <div className="flex justify-center">
+      <a className="w-4/5" href="mailto:devpublic.swiftness382@passfwd.com">
+        <button className="btn btn-ghost bg-gray-100 text-gray-800 w-full">
           <FaEnvelope />
           Email
         </button>
-      </div>
-    </>
+      </a>
+    </div>
   );
 }
