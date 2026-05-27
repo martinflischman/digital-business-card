0d6ae5bff32848cf2b36f42c392323dbad1a2a79
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-05-27 15:47:52 +0200

    feat: add about component
    - add about section with heading and bio paragraph
    - apply padding and typography styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,14 @@
 export default function About() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col justify-start px-6 pt-6">
+        <h2 className="text-lg font-bold mb-2">About</h2>
+        <p className="font-light text-sm opacity-75">
+          I am a fullstack developer with a particular interest in making things
+          simple and minimalist UIs. I try to keep up with security and best
+          practices, and am always looking for new things to learn.
+        </p>
+      </div>
+    </>
+  );
 }
